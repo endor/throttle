11b1b18f06865ed6d73cdaf924a1052a3e5b5e58
Frank Proessdorf <fp@notjusthosting.com>
2015-04-19 16:58:16 +0300

    Throttle requests

--- api.js
+++ api.js
@@ -1,61 +1,140 @@
 var request = require('request');
 var fs = require('fs');
 var Q = require('q');
+var _ = require('underscore');
+
+var queueInterval = null,
+  queue = [],
+  queueLog = [],
+  queueStarted = false,
+  allowedNumberOfRequests = 600,
+  perSeconds = 600;
+
+function countEntriesInLog() {
+  return _.reduce(queueLog, function(count, requests) {
+    return count + requests;
+  }, 0);
+}
+
+function workOffQueue() {
+  if(countEntriesInLog() < allowedNumberOfRequests && queue.length > 0) {
+    var entry = queue.shift();
+    entry.callback(entry.requestOptions, entry.deferred);
+    queueLog[queueLog.length - 1] += 1;
+    workOffQueue();
+  }
+}
+
+function updateLogs() {
+  queueInterval = setInterval(function() {
+    queueLog.shift();
+
+    if(queueLog.length < perSeconds) {
+      queueLog.push(0);
+    }
+
+    workOffQueue();
+  }, 1000);
+}
+
+function startQueue(throttle) {
+  if(!queueStarted) {
+    if(queueInterval) {
+      clearInterval(queueInterval);
+      queueInterval = null;
+    }
+
+    allowedNumberOfRequests = (throttle && throttle.requests) || 600;
+    perSeconds = (throttle && throttle.seconds) || 600;
+
+    queueStarted = true;
+    queue = [];
+    queueLog = [];
+
+    _.each(_.range(perSeconds), function() {
+      queueLog.push(0);
+    });
+
+    updateLogs();
+  }
+}
+
+function logError(err, callback) {
+  fs.appendFile('request_log.txt', err + '\n', function() {
+    callback();
+  });
+}
+
+function throttleRequest(callback, requestOptions, deferred, throttle) {
+  startQueue(throttle);
+  queue.push({callback: callback, requestOptions: requestOptions, deferred: deferred});
+  workOffQueue();
+}
+
+function sendRequest(requestOptions, deferred) {
+  request(requestOptions, function(err, response, body) {
+    if(err) {
+      logError(err, function() {
+        deferred.reject(new Error(err));
+      });
+    } else if(response.statusCode > 206) {
+      var errorMessage = 'Unexpected response ' + response.statusCode + ' for ' +
+        requestOptions.uri + ': ' + response.body;
+      logError(errorMessage, function() {
+        deferred.reject(new Error(errorMessage));
+      });
+    } else {
+      deferred.resolve(body);
+    }
+  });
+}
+
+function makeRequest(verb, sendRequest, uri, data, token, throttle) {
+  var deferred = Q.defer();
+
+  var requestOptions = {
+    method: verb,
+    uri: uri
+  };
+
+  if(data) {
+    requestOptions.form = data;
+  }
+
+  if(token) {
+    requestOptions.qs = {token: token};
+  }
+
+  throttleRequest(sendRequest, requestOptions, deferred, throttle);
+
+  return deferred.promise;
+}
 
 module.exports = {
   endpoint: null,
   token: null,
+  throttle: {
+    requests: 600,
+    seconds: 600
+  },
 
   get: function(path) {
-    return this.request('GET', this.endpoint + path, undefined);
+    return makeRequest('GET', this.sendRequest, this.endpoint + path, undefined, this.token, this.throttle);
   },
   post: function(path, data) {
-    return this.request('POST', this.endpoint + path, data);
+    return makeRequest('POST', this.sendRequest, this.endpoint + path, data, this.token, this.throttle);
   },
   put: function(path, data) {
-    return this.request('PUT', this.endpoint + path, data);
+    return makeRequest('PUT', this.sendRequest, this.endpoint + path, data, this.token, this.throttle);
   },
   delete: function(path) {
-    return this.request('DELETE', this.endpoint + path, undefined);
+    return makeRequest('DELETE', this.sendRequest, this.endpoint + path, undefined, this.token, this.throttle);
   },
 
-  logError: function(err, callback) {
-    fs.appendFile('request_log.txt', err + '\n', function() {
-      callback();
-    });
+  resetQueue: function() {
+    queueStarted = false;
+    startQueue(this.throttle);
   },
 
-  request: function(verb, uri, data) {
-    var deferred = Q.defer();
-
-    var requestOptions = {
-      method: verb,
-      uri: uri
-    };
-
-    if(data) {
-      requestOptions.form = data;
-    }
-
-    if(this.token) {
-      requestOptions.qs = {token: this.token};
-    }
-
-    request(requestOptions, function(err, response, body) {
-      if(err) {
-        this.logError(err, function() {
-          deferred.reject(new Error(err));
-        });
-      } else if(response.statusCode > 206) {
-        var errorMessage = 'Unexpected response ' + response.statusCode + ' for ' + uri + ': ' + response.body;
-        this.logError(errorMessage, function() {
-          deferred.reject(new Error(errorMessage));
-        });
-      } else {
-        deferred.resolve(body);
-      }
-    }.bind(this));
-
-    return deferred.promise;
-  }
+  sendRequest: sendRequest
 };
--- test/api_test.js
+++ test/api_test.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var chai = require('chai');
 var nock = require('nock');
+var sinon = require('sinon');
 var api = require('../api');
 var expect = chai.expect;
 
@@ -36,6 +37,7 @@ describe('api', function() {
     beforeEach(function() {
       api.endpoint = 'http://example.com';
       api.token = null;
+      nock.cleanAll();
     });
 
     describe('make http requests', function() {
@@ -46,19 +48,19 @@ describe('api', function() {
       });
 
       it('allows me to do a post request', function(done) {
-        var scope = nock('http://example.com').post('/campaigns').reply(200);
+        var scope = nock('http://example.com').post('/campaigns').reply(201);
         api.post('/campaigns');
         assertDone(scope, done);
       });
 
       it('allows me to do a put request', function(done) {
-        var scope = nock('http://example.com').put('/campaigns/1').reply(200);
+        var scope = nock('http://example.com').put('/campaigns/1').reply(201);
         api.put('/campaigns/1');
         assertDone(scope, done);
       });
 
       it('allows me to do a delete request', function(done) {
-        var scope = nock('http://example.com').delete('/campaigns/1').reply(200);
+        var scope = nock('http://example.com').delete('/campaigns/1').reply(204);
         api.delete('/campaigns/1');
         assertDone(scope, done);
       });
@@ -69,7 +71,7 @@ describe('api', function() {
         var scope = nock('http://example.com').post('/campaigns', {
           name: 'Some campaign',
           budget: 500
-        }).reply(200);
+        }).reply(201);
         api.post('/campaigns', {
           name: 'Some campaign',
           budget: 500
@@ -81,7 +83,7 @@ describe('api', function() {
         var scope = nock('http://example.com').put('/campaigns/1', {
           name: 'Some campaign',
           budget: 500
-        }).reply(200);
+        }).reply(201);
         api.put('/campaigns/1', {
           name: 'Some campaign',
           budget: 500
@@ -95,7 +97,7 @@ describe('api', function() {
         var scope = nock('http://example.com').post('/campaigns?token=ABC', {
           name: 'Some campaign',
           budget: 500
-        }).reply(200);
+        }).reply(201);
         api.token = 'ABC';
         api.post('/campaigns', {
           name: 'Some campaign',
@@ -107,7 +109,7 @@ describe('api', function() {
 
     describe('return promise', function() {
       it('returns a promise that allows passing a success callback', function(done) {
-        var scope = nock('http://example.com').post('/campaigns').reply(200);
+        var scope = nock('http://example.com').post('/campaigns').reply(201);
         api.post('/campaigns', {
           name: 'Some campaign',
           budget: 500
@@ -143,6 +145,79 @@ describe('api', function() {
             done();
           });
         });
+      });
+    });
+
+    describe('throttle requests', function() {
+      this.timeout(5000);
+
+      beforeEach(function() {
+        sinon.stub(api, 'sendRequest');
+      });
+
+      afterEach(function() {
+        api.sendRequest.restore();
+      });
+
+      it('does not send more requests than allowed', function(done) {
+        api.throttle = {
+          requests: 2,
+          seconds: 4
+        };
+        api.resetQueue();
+        api.post('/campaigns');
+        api.post('/campaigns');
+        api.post('/campaigns');
+
+        setTimeout(function() {
+          expect(api.sendRequest.calledTwice).to.be.true;
+          done();
+        }, 5);
+      });
+
+      it('does not send more requests per time than allowed', function(done) {
+        api.throttle = {
+          requests: 2,
+          seconds: 1
+        };
+        api.resetQueue();
+        api.post('/campaigns');
+        api.post('/campaigns');
+
+        setTimeout(function() {
+          api.post('/campaigns');
+
+          setTimeout(function() {
+            expect(api.sendRequest.calledThrice).to.be.true;
+            done();
+          }, 5);
+        }, 1001);
+      });
+
+      it('handles complex throttling', function(done) {
+        api.throttle = {
+          requests: 2,
+          seconds: 3
+        };
+        api.resetQueue();
+
+        api.put('/campaigns/1', {a: 1});
+        api.put('/campaigns/1', {b: 2});
+        api.put('/campaigns/1', {c: 3});
+
+        expect(api.sendRequest.calledTwice).to.be.true;
+
+        setTimeout(function() {
+          expect(api.sendRequest.calledTwice).to.be.true;
+
+          api.put('/campaigns/1', {d: 4});
+          api.put('/campaigns/1', {e: 5});
+
+          setTimeout(function() {
+            expect(api.sendRequest.callCount).to.eql(4);
+            done();
+          }, 3001);
+        }, 1001);
       });
     });
   });
